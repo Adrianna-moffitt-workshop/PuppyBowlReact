dd9a89cc7a74bbddb52cc5eb67a7bdf23bee388c
Adrianna <adriannamoffitt06@gmail.com>
2023-12-05 13:35:30 -0600

    added Css

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import AllPlayers from "../Components/AllPlayers";
+import NewPlayer from "../Components/NewPlayer";
+
+export default function Homepage() {
+  return (
+    <>
+      <NewPlayer />
+      <AllPlayers />
+    </>
+  );
+}
